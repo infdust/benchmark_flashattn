8d28474e287f843a8a0f8e8807f00c02e8add3a7
yiyang_zhao <69035230+infdust@users.noreply.github.com>
2024-09-20 21:27:33 +0800

    Update benchmark_attn.py

--- benchmark_attn.py
+++ benchmark_attn.py
@@ -1,37 +1,24 @@
+import argparse
 import torch
 import flash_attn_2_cuda as flash_attn
 import torch.cuda.nvtx as nvtx
 
-seqlen = 128
-warmup = 10
-repeat = 100
-heads = 32
-kv_heads = 32
-head_size = 128
-
-qkv = torch.empty((seqlen, heads + kv_heads + kv_heads, head_size), device='cuda', dtype=torch.float16)
-q, k, v = qkv.split([heads, kv_heads, kv_heads], dim=-2)
-seqlens = torch.tensor([0, seqlen], device='cuda', dtype=torch.int32)
-
-start = torch.cuda.Event(enable_timing=True)
-end = torch.cuda.Event(enable_timing=True)
-
-for _ in range(warmup):
+def run_flash_attn(q, k, v, acc_seq_lens, max_seq_len, scale):
     flash_attn.varlen_fwd(
         q,
         k,
         v,
         None,
-        seqlens,
-        seqlens,
+        acc_seq_lens,
+        acc_seq_lens,
         None,
         None,
         None,
         None,
-        seqlen,
-        seqlen,
+        max_seq_len,
+        max_seq_len,
         0.0,
-        head_size**-0.5,
+        scale,
         False,
         True,
         -1,
@@ -40,36 +27,45 @@ for _ in range(warmup):
         False,
         None,
     )
-torch.cuda.synchronize()
-nvtx.range_push("benchmark")
-start.record()
-for _ in range(repeat):
-    flash_attn.varlen_fwd(
-        q,
-        k,
-        v,
-        None,
-        seqlens,
-        seqlens,
-        None,
-        None,
-        None,
-        None,
-        seqlen,
-        seqlen,
-        0.0,
-        head_size**-0.5,
-        False,
-        True,
-        -1,
-        -1,
-        0.0,
-        False,
-        None,
-    )
-end.record()
-torch.cuda.synchronize()
-nvtx.range_pop()
+    
+def run(acc_seq_lens, max_seq_len, total_seq_len, q_heads, kv_heads, head_size, warmup, repeat):
+    qkv = torch.empty((total_seq_len, q_heads + kv_heads + kv_heads, head_size), device='cuda', dtype=torch.float16)
+    q, k, v = qkv.split([q_heads, kv_heads, kv_heads], dim=-2)
+    seqlens = torch.tensor(acc_seq_lens, device='cuda', dtype=torch.int32)
 
-duration = start.elapsed_time(end) * 1e3 / repeat
-print(f"Operator duration: {duration:.2f} us")
+    start = torch.cuda.Event(enable_timing=True)
+    end = torch.cuda.Event(enable_timing=True)
+
+    for _ in range(warmup):
+        run_flash_attn(q, k, v, acc_seq_lens, max_seq_len, head_size**-0.5)
+
+    torch.cuda.synchronize()
+    nvtx.range_push("benchmark")
+    start.record()
+    for _ in range(repeat):
+        run_flash_attn(q, k, v, acc_seq_lens, max_seq_len, head_size**-0.5)
+    end.record()
+    torch.cuda.synchronize()
+    nvtx.range_pop()
+    return start.elapsed_time(end) * 1e3 / repeat
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--q-heads', type=int, default=32)
+    parser.add_argument('--kv-heads', type=int, default=32)
+    parser.add_argument('--head-size', type=int, default=128)
+    parser.add_argument('--warmup', type=int, default=10)
+    parser.add_argument('--repeat', type=int, default=100)
+    parser.add_argument('--seq-lens', type=str, default='128')
+    seq_lens = [int(s) for s in numbers_str.split(',')] if args.seq_lens else []
+    assert len(seq_lens) > 0
+    max_seq_len = 0
+    total_seq_len = 0
+    acc_seq_lens = []
+    for seq_len in seq_lens:
+        if seq_len > max_seq_len:
+            max_seq_len = seq_len
+        total_seq_len += seq_len
+        acc_seq_lens.append(total_seq_len)
+    duration = run(acc_seq_lens, max_seq_len, total_seq_len, args.q_heads, args.kv_heads, args.head_size, args.warmup, args.repeat)
+    print(f"Operator duration: {duration:.2f} us")
