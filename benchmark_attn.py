ccc42e30696da828245184bf7c7067f3d0602970
yiyang_zhao <69035230+infdust@users.noreply.github.com>
2024-09-20 10:12:59 +0800

    Update benchmark_attn.py

--- benchmark_attn.py
+++ benchmark_attn.py
@@ -1,5 +1,4 @@
 import torch
-import time
 import flash_attn_2_cuda as flash_attn
 
 seqlen = 128 * 4
@@ -12,6 +11,9 @@ head_size = 128
 qkv = torch.empty((seqlen, heads + kv_heads + kv_heads, head_size), device='cuda', dtype=torch.float16)
 q, k, v = qkv.split([heads, kv_heads, kv_heads], dim=-2)
 seqlens = torch.tensor([0, seqlen], device='cuda', dtype=torch.int32)
+
+start = torch.cuda.Event(enable_timing=True)
+end = torch.cuda.Event(enable_timing=True)
 
 for _ in range(warmup):
     flash_attn.varlen_fwd(
@@ -38,7 +40,7 @@ for _ in range(warmup):
         None,
     )
 torch.cuda.synchronize()
-start_time = time.time()
+start.record()
 for _ in range(repeat):
     flash_attn.varlen_fwd(
         q,
@@ -64,7 +66,7 @@ for _ in range(repeat):
         None,
     )
 torch.cuda.synchronize()
-end_time = time.time()
+end.record()
 
-duration = (end_time - start_time) * 1e6 / repeat
+duration = start.elapsed_time(end) * 1e6 / repeat
 print(f"Operator duration: {duration:.2f} us")
